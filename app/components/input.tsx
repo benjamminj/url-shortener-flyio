dfa09bb26791dab1ee53c851859aea229baa234c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-04-18 12:16:47 -0700

    fix: value

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -9,7 +9,6 @@ export const Input = ({ label, ...props }: InputProps) => {
         className="peer inline-block rounded-none border-2 p-2 pt-6 focus:outline-none dark:border-slate-500 dark:bg-transparent focus:dark:border-slate-50"
         placeholder=" "
         {...props}
-        defaultValue=""
       />
       <span className="absolute left-2 top-2 ml-[1px] text-xs uppercase">
         {label}
